5343efb639398f74c6388d98914ad4acba6f4ee0
boyxp <isme@live.com>
2016-03-06 22:06:25 +0800

    新增connection接口pdo实现

---
+++ src/connection/adapter/pdo.php
@@ -0,0 +1,44 @@
+<?php
+namespace bluefin\orm\connection\adapter;
+use bluefin\orm\connection\connection as connectionInterface;
+class pdo extends \PDO implements connectionInterface
+{
+	public function __construct($dsn, $user='', $password='', array $options=null)
+	{
+		if(strpos(strtolower($dsn), 'charset=')!==false) {
+			preg_match('/charset=([a-z0-9-]+)/i', $dsn, $match);
+			$charset = isset($match[1]) ? $match[1] : 'utf8';
+		} else {
+			$charset = isset($options['charset']) ? $options['charset'] : 'utf8';
+			$dsn    .= (substr($dsn, -1)===';' ? '' : ';')."charset={$charset}";
+		}
+
+		try {
+			parent::__construct($dsn, $user, $password, array(\PDO::ATTR_PERSISTENT => true));
+		} catch (exception $e) {
+			throw new \InvalidArgumentException('Connection failed: '.$e->getMessage(), $e->getCode());
+		}
+
+		$this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		$this->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+
+		$timezone = isset($options['timezone']) ? $options['timezone'] : '+08:00';
+		$this->exec("SET time_zone='{$timezone}'");
+		$this->exec("SET NAMES '{$charset}'");
+	}
+
+	public function begin():bool
+	{
+		return parent::beginTransaction();
+	}
+
+	public function commit():bool
+	{
+		return parent::commit();
+	}
+
+	public function rollback():bool
+	{
+		return parent::rollback();
+	}
+}
