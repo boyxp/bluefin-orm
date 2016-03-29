3cd1e2d8aaa74d13b0bbe5c927a5d344d080fd85
boyxp <isme@live.com>
2016-03-29 21:58:03 +0800

    新增toArray()方法

--- src/resultset/resultset.php
+++ src/resultset/resultset.php
@@ -7,7 +7,8 @@ interface resultset extends Countable,Iterator,ArrayAccess
 {
 	public function offset(int $row=0, int $offset=0);
 	public function column(string $column=null, string $index=null):array;
-	public function each(callable $callback):array;
-	public function map(callable $callback):array;
-	public function join(resultset $result, string $leftKey=null, string $rightKey=null):array;
+	public function each(callable $callback):resultset;
+	public function map(callable $callback):resultset;
+	public function join(resultset $result, string $leftKey=null, string $rightKey=null):resultset;
+	public function toArray():array;
 }
